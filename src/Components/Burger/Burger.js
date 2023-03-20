db8111113baad202c448bdd3e1f677757feb1de6
Patricio Sala <patricio.sala.ext@errepar.com>
2023-03-20 17:46:30 -0300

    responsive header

---
+++ src/Components/Burger/Burger.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Burger.css";
+
+function Burger() {
+    return (
+        <div className="burger-container">
+            <div className="tile" id="1"></div>
+            <div className="tile" id="2"></div>
+            <div className="tile" id="3"></div>
+        </div>
+    )
+}
+
+export default Burger;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Header.css";
 
+// Components
+import Burger from "../Burger/Burger";
+
 function Header() {
     return (
-        <div className="header-container"></div>
+        <div className="header-container">
+            <Burger/>
+        </div>
     )
 }
 
